dfaa8e8f230feb23b4518b0fb7d87c54430c3b0c
akmsajjad <you@akmsajjad.com>
2022-06-16 21:46:43 +0600

    all new files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 mx-auto">
+            <h1 className="text-center mx-auto text-white pt-4 pb-4">TODO APP</h1>
+            <div className="row">
+            <div className="col-9 text-light">
+              <input type="text" className="form-control text-light" placeholder='My Daily Workflow...' />
+            </div>
+            <div className="col-3 ">
+              <button className="btn btn-warning text-bold">Add</button>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
